c89e032fcc563344f70707a9c240e2a03dafefc6
Tom Haynes <tomhaynes94@gmail.com>
2022-04-18 21:12:29 +0100

    added setSail() method to Ship object. Changed to class syntax

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -10,4 +10,12 @@ describe('Ship', () => {
         
         expect(ship.startingPort).toBe('Dover');
     });
+
+    it('can set sail', () => {
+        const ship = new Ship('Dover');
+
+        ship.setSail();
+
+        expect(ship.startingPort).toBeFalsy();
+    });
 });
--- src/ship.js
+++ src/ship.js
@@ -1,10 +1,25 @@
 const startingPort = 'Dover';
 
-function Ship(name) {
-    this.name = name;
-    this.startingPort = startingPort;
+class Ship {
+    constructor(name) {
+        this.name = name;
+        this.startingPort = startingPort;
+    }
 
+    setSail() {
+        this.startingPort = null;
+    }
 };
+
+// function Ship(name) {
+//     this.name = name;
+//     this.startingPort = startingPort;
+
+// };
+
+// Ship.prototype.setSail = function() {
+//     this.startingPort = null;
+// };
 
 
 module.exports = Ship;
